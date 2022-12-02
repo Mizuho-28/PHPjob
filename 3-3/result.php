d432def31f9d1f4e7d9196965178ff17de8207b9
Mizuho-28 <118008306+Mizuho-28@users.noreply.github.com>
2022-12-02 23:57:42 +0900

    Create result.php

---
+++ 3-3/result.php
@@ -0,0 +1,20 @@
+<?php
+$text = $_POST['text'];
+$substr = substr($text,-mt_rand(1,10),1)
+?>
+<p><?php echo date("Y/m/d")?>の運勢は</p>
+<p>選ばれた数字は<?php echo $substr ?></p>
+
+<?php
+  if($substr==0){
+ echo "凶";
+} elseif($substr==1 ||$substr==2 || $substr==3){
+ echo "小吉";
+} elseif($substr==4 ||$substr==5 || $substr==6){
+  echo "中吉";
+} elseif($substr==7 ||$substr==8 ){
+  echo "吉";
+} elseif($substr ==9){
+  echo "大吉";
+}
+?>
