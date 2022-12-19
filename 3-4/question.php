9795f181853d0f0ef27c26e9c16cf27fdf02e5f9
Mizuho-28 <118008306+Mizuho-28@users.noreply.github.com>
2022-12-19 18:45:38 +0900

    Update question.php

--- 3-4/question.php
+++ 3-4/question.php
@@ -4,21 +4,24 @@
 $text = $_POST['text'];
 //①画像を参考に問題文の選択肢の配列を作成してください。
 
-//② ①で作成した、配列から正解の選択肢の変数を作成してください
+//★② ①で作成した、配列から正解の選択肢の変数を作成してください
+$ports_correct = 80;
+$languages_correct = "HTML";
+$commands_correct = "select";
 ?>
-<p>お疲れ様です<?php echo $text;?><!--POST通信で送られてきた名前を出力-->さん</p>
-<!--フォームの作成 通信はPOST通信で-->
-<h2>①ネットワークのポート番号は何番？</h2>
+
+<form action="answer.php" method="post">
+  <p>お疲れ様です<?php echo $text;?><!--POST通信で送られてきた名前を出力-->さん</p>
+  <!--フォームの作成 通信はPOST通信で-->
+  <h2>①ネットワークのポート番号は何番？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 
 <?php
 $ports = [80,22,20,21];
 foreach($ports as $value){ ?>
-   <form action="answer.php" method="post">
-      <input type="radio" name="ports">
-        <?php echo $value;
-      }?>
-      </form> 
+  <input type="radio" name="ports">
+       <?php echo $value;
+      }?>     
 
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
@@ -39,8 +42,11 @@ foreach($commands as $value){ ?>
   <input type="radio" name="commands">
  <?php echo $value;
 }
-?>
+?> 
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
-<form action="answer.php" method="get">
-  <input type="submit" value="回答する" />
+<input type="hidden" name="ports_correct" value="<?php echo $ports_correct; ?>">
+<input type="hidden" name="ports_correct" value="<?php echo $languages_correct; ?>">
+<input type="hidden" name="ports_correct" value="<?php echo $commands_correct; ?>">
+<input type="hidden" name="text" value="<?php echo $text; ?>"><br>
+<input type="submit" value="回答する" />
 </form>
