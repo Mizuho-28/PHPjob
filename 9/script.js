ac8d62fd651bc2d8ee7d1ecd4cef2be235e548d0
Mizuho-28 <118008306+Mizuho-28@users.noreply.github.com>
2023-01-23 01:45:26 +0900

    Create script.js

---
+++ 9/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.click').click(function(){
+    $('.left').animate({width:'0%'},2000);
+    $('.left').animate({width:'50%'},2000);
+    $('.right').animate({width:'0%'},2000);
+    $('.right').animate({width:'50%'},2000);
+  }
+
+  )
+
+});
