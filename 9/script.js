e1a21cadcc48bb00ed108cdcd1417f99737f4550
Mizuho-28 <118008306+Mizuho-28@users.noreply.github.com>
2023-01-23 23:15:13 +0900

    Update script.js

--- 9/script.js
+++ 9/script.js
@@ -1,11 +1,8 @@
 $(function(){
   $('.click').click(function(){
-    $('.left').animate({width:'0%'},2000);
-    $('.left').animate({width:'50%'},2000);
-    $('.right').animate({width:'0%'},2000);
-    $('.right').animate({width:'50%'},2000);
-  }
-
-  )
-
+   
+  $('.left').toggleClass('action');
+  $('.right').toggleClass('action');
+  
+  }); 
 });
